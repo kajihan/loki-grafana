5d882b5a33e0e002ecd38f06ea761dcb63029abe
kajihan <141428968+kajihan@users.noreply.github.com>
2025-03-27 19:29:13 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
-    "github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4"
 	"webapp/pkg/config"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+	// Set up logging with JSON format for Loki
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetLevel(log.InfoLevel)
+
+	// Get port from environment variable, default to 3000
 	port := config.GetEnv("PORT", "3000")
 
-    e := echo.New()
-    e.Static("/", "public")
-    
-    e.GET("/", func(c echo.Context) error {
-        return c.File("public/views/webapp.html")
-    })
-    e.Start(":" + port)
+	// Initialize Echo
+	e := echo.New()
+	e.Static("/", "public")
+
+	// Define root route
+	e.GET("/", func(c echo.Context) error {
+		log.WithFields(log.Fields{
+			"method": c.Request().Method,
+			"path":   c.Request().URL.Path,
+		}).Info("Serving webapp.html")
+		return c.File("public/views/webapp.html")
+	})
+
+	// Start the server
+	log.WithField("port", port).Info("Starting web application")
+	if err := e.Start(":" + port); err != nil {
+		log.WithError(err).Fatal("Failed to start server")
+	}
 }
